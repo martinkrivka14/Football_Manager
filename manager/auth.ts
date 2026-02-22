2741b74a63b068a05ccb07a37fa21ec3a702fd06
martinkrivka14 <mkrivka14@gmail.com>
2026-02-22 13:55:25 +0100

    installing next-auth
    installed next-auth for authentication

---
+++ manager/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
+
+export const {auth, handlers, signIn, signOut} = NextAuth({
+  providers: [Github({
+    clientId: process.env.AUTH_GITHUB_ID!,
+    clientSecret: process.env.AUTH_GITHUB_SECRET!,
+  })],}); 
